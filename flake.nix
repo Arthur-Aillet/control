96259d788f92d9d55268a23f4cbe8ced49197425
Axel Denis <axel.denis@epitech.eu>
2025-11-23 14:16:38 +0000

    feat: adding to flake

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
         routing = mkModule ./modules/routing.nix;
         pihole = mkModule ./modules/pihole.nix;
         siyuan = mkModule ./modules/siyuan.nix;
+        seafile = mkModule ./modules/seafile.nix;
 
         default =
           { lib, ... }:
@@ -45,6 +46,7 @@
               self.nixosModules.routing
               self.nixosModules.pihole
               self.nixosModules.siyuan
+              self.nixosModules.seafile
             ];
 
             options.control = {
